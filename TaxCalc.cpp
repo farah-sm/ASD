6f5f0c5a1b1fbfd0eadad3c0c72079e39731a80c
Farah s <65547726+farah-sm@users.noreply.github.com>
2023-01-30 19:09:54 +0000

    Create TaxCalc.cpp

---
+++ TaxCalc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    
+    double sales = 95000;
+    double stateTax = 4;
+    double countyTax = 2; 
+    
+    double stSales = (stateTax / 10) * sales;
+    double ctSales = (countyTax / 10) * sales;
+ 
+    
+    
+    cout << "Total sales = $" << sales << endl
+         << "Sales after State Tax Deduction = $" << stSales << endl
+         << "Sales after County Tax Deduction = $" << ctSales;
+}
