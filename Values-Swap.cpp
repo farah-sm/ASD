5125368dea735fff01a0a84156f4e60bbcb2157b
Farah s <65547726+farah-sm@users.noreply.github.com>
2023-01-30 14:11:06 +0000

    Create Values-Swap.cpp

---
+++ Values-Swap.cpp
@@ -0,0 +1,20 @@
+// Include one of the files in the standard library 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // cout stands for character output
+    
+    int a = 1;
+    int b = 2;
+    int c = a;
+
+    a = b;
+    b = c;
+  
+    
+    cout << b;
+    return 0;
+   
+}
