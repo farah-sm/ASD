9085b39f4a38ce7bc7a8125e713548d7583cf0fc
Farah s <65547726+farah-sm@users.noreply.github.com>
2023-01-31 11:00:30 +0000

    Create AutoKeyword.cpp

---
+++ OutOfHours/AutoKeyword.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    
+    // Long
+    auto longg = 3000L;
+    // Float
+    auto floaat = 5342F;
+    // Int
+    auto innt = 8937;
+    // Double
+    auto doubl = 2398.0;
+    
+   
+}
