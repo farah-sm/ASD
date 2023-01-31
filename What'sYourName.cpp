468f60b39bf3764bb567f2c8291f7a0653bcdd14
Farah s <65547726+farah-sm@users.noreply.github.com>
2023-01-31 07:33:06 +0000

    Create What'sYourName.cpp

---
+++ What'sYourName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    // display question to screen
+    cout << "How old are you: ";
+    // Declare a varian;e
+    int age;
+    // Initialise the value of the age variable with the character input >>
+    cin >> age;
+    // Display and print to the console the concatonated sentence with ~ age = variable
+    cout << "I am " << age << " years old.";
+   
+}
