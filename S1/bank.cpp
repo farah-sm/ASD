b9545329af0f1bdb59b1a7166bebe686a4e70f4f
Farah s <65547726+farah-sm@users.noreply.github.com>
2023-02-01 14:35:35 +0000

    Update bank.cpp

--- S1/bank.cpp
+++ S1/bank.cpp
@@ -3,45 +3,48 @@ using namespace std;
 int main() {
     // Write C++ code here
     float balance;
-    
+
     cout << "What is your balance: ";
-    cin >> balance; 
-    cout <<"Your current balance is: " << balance<<endl;
+    cin >> balance;
+    cout << "Your current balance is: "; 
+    // using the C way of decimal placement, the method prinf is from C
+    printf("% .6f", balance);
+    cout << "\n";
     float deposit;
     cout << "Please enter your deposit amount: ";
     cin >> deposit;
-    
+
     float newBalance = balance + deposit;
-    
-    cout << "Your new balance is: " << newBalance<<endl;
+
+    cout << "Your new balance is: " << newBalance << endl;
 
     float withdraw;
     cout << "How much would you like to withdraw: ";
     cin >> withdraw;
-    
+
     if (withdraw < newBalance)
     {
-        
+
         newBalance = newBalance - withdraw;
-    
-        cout<< "Your balance after you've withdrawn is: "<< newBalance<<endl;
+
+        cout << "Your balance after you've withdrawn is: " << newBalance << endl;
     }
     else
     {
-        cout << "Sorry baya, you are broke"<<endl;
+        cout << "Sorry baya, you are broke" << endl;
     }
-    
-    
-  
+
+
+
     double stateTax = 2.3;
     double temp = newBalance;
-   newBalance = (stateTax / 10) * newBalance + temp;
- 
-    
+    newBalance = (stateTax / 10) * newBalance + temp;
+
+
     cout << "Total after adding tax rates = " << newBalance << endl;
-        
-   
-   
-   
+
+
+
+
     return 0;
 }
